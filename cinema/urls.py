0ffb21a1a91fbb894562167daa96898189fd654f
Roman Vynnytskyi <roman.vynnytskyi.tt@gmail.com>
2024-12-04 22:31:06 +0200

    write movie_list and movie_detail in views.py

---
+++ cinema/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from cinema.views import movie_list, movie_detail
+
+urlpatterns = [
+    path('movies/', movie_list, name='movie-list'),
+    path('movies/<int:pk>/', movie_detail, name='movie-detail'),
+]
